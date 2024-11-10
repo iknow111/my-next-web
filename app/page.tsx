f5d42e0386e520cccd6c49d1fa5f8fc8b9b1652c
iknow111 <psy4588213@gmail.com>
2024-11-10 15:58:28 +0900

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,5 @@
 export default function Home() {
   return (
-    <div>
-      <MyButton name = "yeobi" nickname = "y" today={new Date()}/>
-    </div>
+    <div>This is my home page</div>
   );
 }
-
-// function MyButton({
-//   name
-// }: {
-//   name: string
-// }) {
-//   return <button>I am {name}</button>
-// }
-
-type TParams = {
-  name: string
-  nickname : string
-  today : Date
-}
-
-function MyButton(params: TParams){
-  return (
-
-   <div className="bg-blue-500 text-xl w-[100px]" style={{backgroundColor: "rgb(203 213 255)"}}>
-    I am {params.name}. my nickname is {params.nickname}. {" "}
-    </div>
-  );
-};
-
